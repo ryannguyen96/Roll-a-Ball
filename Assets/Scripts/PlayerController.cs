eb477d345bec148c700da61a178b6805c402385b
Ryan Nguyen <shiga2714@gmail.com>
2018-09-13 21:41:00 -0400

    Pick Up objects work as intended
    - Pick Up objects can now be collected by the Player.
    - A Trigger Event was added onto the script of the Player object to interact with the Pick Up objects.
    - Pick Up object is now tied to the "Pick Up" tab.
    - Pick Up objects now have a Rigidbody and are Kinematic.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour {
 
     private Rigidbody rb;
     public float speed;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -19,5 +20,11 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+            other.gameObject.SetActive(false);
     }
 }
