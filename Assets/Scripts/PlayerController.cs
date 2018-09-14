8173dfedd9816966162f2e6b49349687e751f9ea
Ryan Nguyen <shiga2714@gmail.com>
2018-09-13 21:59:07 -0400

    Added counter and win text
    - PlayerController script now has been modified to hold a counter for each Pick Up object collected and displays Win text when all objects are collected.
    - Implemented into Unity the Text game objects.
    - Modified the Text to appear in top left corner and above the Player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody rb;
     public float speed;
+    public Text countText;
+    public Text winText;
+    private int count;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     void FixedUpdate(){
@@ -25,6 +32,22 @@ public class PlayerController : MonoBehaviour {
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Pick Up"))
+        {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+        
     }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 15)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
+
 }
